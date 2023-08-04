2c84e5ca559cc6c572f182bca716bdc40c1ba05e
jenish2060 <114239379+jenish2060@users.noreply.github.com>
2023-08-04 22:26:16 +0545

    Added basic routing

--- src/pages/pageNotFound.js
+++ src/pages/pageNotFound.js
@@ -1,7 +1,7 @@
-export default function Admin() {
+export default function PageNotFound() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Admin
+      Page Not Found
     </h1>
   )
 }
